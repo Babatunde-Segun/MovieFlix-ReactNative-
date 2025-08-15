70ecc635afcbb2beba101b079b52a33edd540aab
Babatunde-Segun <officiallyvincify@gmail.com>
2025-08-15 09:38:25 +0100

    appwrite.ts added

---
+++ services/appwrite.ts
@@ -0,0 +1,8 @@
+// track the searches made by  a user
+
+export const udateSearchCount = async (query: string, movie: Movie) => {
+  // check if a record of that search has already been stored
+  // if a document is found increment the searchCount field
+  // if no document is found c
+  // create a new document in apprwirte datatbe 
+}
